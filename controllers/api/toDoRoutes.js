6bb681bc3277e575a48cac0c02d39f479d20058d
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-12 21:43:22 -0400

    get route for api/todos returns data

--- controllers/api/toDoRoutes.js
+++ controllers/api/toDoRoutes.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
+const { User, Child, devMilestones, medMilestones } = require('../../models')
 
 
 //TODO - Render ALL upcoming milestones and appointments
 router.get('/', async (req, res) => {
     try {
+        // res.send('<h1>Tiny Triumphs Landing Page</h1>');
+        const childData = await Child.findAll({
+            include: [
+                {
+                    model: User,
+                    arributes: ['name'],
+                },
+            ],
+        });
 
+        const children = childData.map((children) => children.get({ plain: true }));
+
+        //TODO change to render when handlebars is functional
+        res.send(children);
     } catch (err) {
     res.status(400).json(err);
     }
