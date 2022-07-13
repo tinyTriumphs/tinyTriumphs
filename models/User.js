c926839920b80d6935c219dad5c0ed39904262da
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-12 20:04:53 -0500

    added hooks to user model and spacing on userRoutes updated

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 ///@TOM this is a direct copy from mini project
-
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
