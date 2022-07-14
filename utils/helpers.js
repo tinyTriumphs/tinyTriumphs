a6d326d3f5afc179ce61e5cb1103c94598b21d7d
kkmusau <kkmusau@gmail.com>
2022-07-14 18:53:54 -0500

    added user login

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,16 +1,16 @@
 // module.exports = {
 //   format_date: (date) => {
-//     // Format date as MM/DD/YYYY
+//     Format date as MM/DD/YYYY
 //     return date.toLocaleDateString();
 //   },
 //   format_amount: (amount) => {
-//     // format large numbers with commas
+//     format large numbers with commas
 //     return parseInt(amount).toLocaleString();
 //   },
 //   get_emoji: () => {
 //     const randomNum = Math.random();
 
-//     // Return a random emoji
+//     Return a random emoji
 //     if (randomNum > 0.7) {
 //       return `<span for="img" aria-label="lightbulb">💡</span>`;
 //     } else if (randomNum > 0.4) {
