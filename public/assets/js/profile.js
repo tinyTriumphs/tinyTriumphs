b6b772b0af5faaf441dec9439f229b89f574bd1a
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-15 23:19:37 -0500

    updated profile.handlebars form to add child for the user, as well as profile.js

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -3,51 +3,51 @@
 
 
 /////SAMPLE FROM MINIPROJECT
-// const newFormHandler = async (event) => {
-//     event.preventDefault();
+const newFormHandler = async (event) => {
+    event.preventDefault();
   
-//     const name = document.querySelector('#project-name').value.trim();
-//     const needed_funding = document.querySelector('#project-funding').value.trim();
-//     const description = document.querySelector('#project-desc').value.trim();
+    const name = document.querySelector('#child-name').value.trim();
+    const childBirthDate = document.querySelector('#child-birthdate').value.trim();
+    const gender = document.querySelector('#child-gender').value.trim();
   
-//     if (name && needed_funding && description) {
-//       const response = await fetch(`/api/projects`, {
-//         method: 'POST',
-//         body: JSON.stringify({ name, needed_funding, description }),
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       });
+    if (name && childBirthDate && gender) {
+      const response = await fetch(`/api/projects`, {
+        method: 'POST',
+        body: JSON.stringify({ name, childBirthdate, gender }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
   
-//       if (response.ok) {
-//         document.location.replace('/profile');
-//       } else {
-//         alert('Failed to create project');
-//       }
-//     }
-//   };
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
   
-//   const delButtonHandler = async (event) => {
-//     if (event.target.hasAttribute('data-id')) {
-//       const id = event.target.getAttribute('data-id');
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
   
-//       const response = await fetch(`/api/projects/${id}`, {
-//         method: 'DELETE',
-//       });
+      const response = await fetch(`/api/projects/${id}`, {
+        method: 'DELETE',
+      });
   
-//       if (response.ok) {
-//         document.location.replace('/profile');
-//       } else {
-//         alert('Failed to delete project');
-//       }
-//     }
-//   };
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
   
-//   document
-//     .querySelector('.new-project-form')
-//     .addEventListener('submit', newFormHandler);
+  document
+    .querySelector('.new-child-form')
+    .addEventListener('submit', newFormHandler);
   
-//   document
-//     .querySelector('.project-list')
-//     .addEventListener('click', delButtonHandler);
+  document
+    .querySelector('.project-list')
+    .addEventListener('click', delButtonHandler);
   
