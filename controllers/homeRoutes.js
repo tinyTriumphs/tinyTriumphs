fd6a3d443ce4408ca3c084dac2756b32c7de9217
kkmusau <kkmusau@gmail.com>
2022-07-15 22:46:43 -0500

    added profile route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
 /////SAMPLE FROM MINI PROJECT/////
-// const { Project, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const { Project, User, devMilestones } = require('../models');
+const withAuth = require('../utils/auth');
 
 
 // TODO: Alex
 router.get('/', async (req, res) => {
   try {
     res.render('homepage', { 
-            // projects, 
+      //       projects, 
       // logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -19,29 +19,31 @@ router.get('/', async (req, res) => {
 
 // TODO: Tom
 // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
+router.get('/profile', withAuth, async (req, res) => {
+  try {
 
 // //TODO: Tom
 
-//     //////// SAMPLE FROM MINI PROJECT ////////
-//      // Find the logged in user based on the session ID
-
-//      const userData = await User.findByPk(req.session.user_id, {
-//         attributes: { exclude: ['password'] },
-//         include: [{ model: Project }],
-//       });
+    //////// SAMPLE FROM MINI PROJECT ////////
+     // Find the logged in user based on the session ID
+debugger
+console.log(req.session.user_id);
+     const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        // include: [{ model: devMilestones }],
+      });
+  console.log(userData);
+      const user = userData.get({ plain: true });
+      
   
-//       const user = userData.get({ plain: true });
-  
-//       res.render('profile', {
-//         ...user,
-//         logged_in: true
-//     //   });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 // TODO: Tom
