e3e4d647bc23cebe8ad1b6ab1525e851e74d828e
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-15 22:12:33 -0500

    profile route now correctly renders user-specific information

--- public/assets/js/logout.js
+++ public/assets/js/logout.js
@@ -3,18 +3,18 @@
 
 ///SAMPLE FROM MINI PROJECT////
 
-// const logout = async () => {
-//     const response = await fetch('/api/users/logout', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//     });
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
   
-//     if (response.ok) {
-//       document.location.replace('/');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   };
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
   
-//   document.querySelector('#logout').addEventListener('click', logout);
+  document.querySelector('#logout').addEventListener('click', logout);
   
