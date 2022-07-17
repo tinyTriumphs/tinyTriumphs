523709932bd8c8005c2cd6e5c6ee762eef077df7
Tommy Lee <t.k.hobbes43@gmail.com>
2022-07-17 12:47:05 -0500

    more tada routes

--- controllers/api/taDaRoutes.js
+++ controllers/api/taDaRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { Child, devMilestones } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // the '/api/tada' endpoint
 
-//TODO - Render ALL COMPLETED milestones and appointments
+//TODO - Render ALL COMPLETED milestones
 router.get('/', async (req, res) => {
     try {
         const taDaData = await Child.findAll({
@@ -20,6 +21,30 @@ router.get('/', async (req, res) => {
         res.status(200).json(taDaData);
     } catch (err) {
     res.status(500).json(err);
+    }
+});
+
+// render all completed milestones by USER's Children
+router.get('/:user_id', withAuth, async (req, res) => {
+    try {
+        const tadaData = await Child.findAll({
+            include: [
+                {
+                    model: devMilestones,
+                    attributes: ['id', 'devMilestone', 'devMilestone_complete', 'devMilestone_expectedDate', 'devMilestone_expectedRange', 'devMilestone_dateComplete', 'child_id'],
+                    where: {
+                        devMilestone_complete: true
+                    }
+                }
+            ]
+        });
+        if (!tadaData) {
+            res.status(404).json({ message: "No completed TaDa's found with this user!" });
+            return;
+        }
+        res.status(200).json(tadaData);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
