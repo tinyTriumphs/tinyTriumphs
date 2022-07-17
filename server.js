862481e06808e165ab64b9bb0074b813c952a080
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-17 15:25:10 -0500

    added multer package and upload functionality through insomnia

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const multer = require('multer');
+const uuid = require('uuid').v4;
+
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -35,6 +39,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+   filename: (req, file, cb) => {
+    const { originalname } = file;
+    cb(null, `${uuid()}-${originalname}`);
+   }
+})
+
+const upload = multer({storage});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file)
+  res.json({status: "success"}); 
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
