c139324eda3160d9c0c85729b12477cc997a8785
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-18 11:43:58 -0500

    updated children profile page w/ settings link and working handlebars for profile page

--- public/assets/js/photo.js
+++ public/assets/js/photo.js
@@ -6,14 +6,25 @@ const newFormHandler = async (event) => {
     
     var uploadButtonClicked = $(event.target);
     currentInput = uploadButtonClicked.parent()[0];
+    const selectedFile = document.getElementById('input').files[0].name;
+    console.log (selectedFile);
     console.log('it was clicked');
     console.log(currentInput);
-    await alert(currentInput)
+
+    // if (currentInput) {
+    //     const response = await fetch(`/upload`, {
+    //         method: 'POST',
+    //         body: 
+    //     })
+    // }
 
 
     // $('#child-photo-upload').attr('src', 'new');
 }
 
+// document
+//     .querySelector('#photo-upload')
+//     .addEventListener('submit', newFormHandler)
 
 // const newFormHandler = async (event) => {
 //     event.preventDefault();
