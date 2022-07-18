da805e6f70a1b9927f0004b41c186613f698803a
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-18 15:04:45 -0500

    todos and tadas landing pages functional

--- controllers/api/taDaRoutes.js
+++ controllers/api/taDaRoutes.js
@@ -1,7 +1,36 @@
 const router = require('express').Router();
-const { Child, devMilestones, medMilestones } = require('../../models');
+const { User, Child, devMilestones, medMilestones } = require('../../models');
 
 // the '/api/tada' endpoint
+router.get("/", async (req, res) => {
+    try {
+      //Finds all children associated w/ logged in user id
+      const childData = await Child.findAll(
+        {
+        include: [
+          {
+            model: User,
+            attributes: ["id"],
+            where: {
+              id: req.session.user_id,
+            },
+          },
+        ],
+      }
+      );
+  
+      const childs = childData.map((childs) => childs.get({ plain: true }));
+
+      res.render("tadas", {
+        childs,
+        logged_in: req.session.logged_in,
+        user_id: req.session.user_id,
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
 
 //TODO - Render ALL COMPLETED milestones and appointments
 // router.get('/', async (req, res) => {
