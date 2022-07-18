55e70943b460455a3fd5c37842ba74d4dc818fd3
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-18 14:11:01 -0500

    tadas route rendering a table if there is data in med and dev milestones

--- controllers/api/taDaRoutes.js
+++ controllers/api/taDaRoutes.js
@@ -4,24 +4,32 @@ const { Child, devMilestones, medMilestones } = require('../../models');
 // the '/api/tada' endpoint
 
 //TODO - Render ALL COMPLETED milestones and appointments
-router.get('/', async (req, res) => {
-    try {
-        const taDaData = await Child.findAll({
-            include: [
-                {
-                    model: devMilestones,
-                    attributes: ['id', 'devMilestone', 'devMilestone_complete', 'devMilestone_expectedDate', 'devMilestone_expectedRange', 'devMilestone_dateComplete', 'child_id'],
-                    where: {
-                        devMilestone_complete: true
-                    }
-                }
-            ]
-        });
-        res.status(200).json(taDaData);
-    } catch (err) {
-    res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         const taDaData = await Child.findAll({
+//             include: [
+//                 {
+//                     model: devMilestones,
+//                     attributes: ['id', 'devMilestone', 'devMilestone_complete', 'devMilestone_expectedDate', 'devMilestone_expectedRange', 'devMilestone_dateComplete', 'child_id'],
+//                     where: {
+//                         devMilestone_complete: true
+//                     }
+//                 }
+//             ]
+//         });
+//         res.status(200).json(taDaData);
+
+//         const childTaDas = taDaData.get({ plain: true });
+
+//         res.render("childidtadas", {
+//             ...childTaDas,
+//             logged_in: true,
+//           });
+
+//     } catch (err) {
+//     res.status(500).json(err);
+//     }
+// });
 
 router.get('/:id', async (req, res) => {
     try {
@@ -41,7 +49,15 @@ router.get('/:id', async (req, res) => {
                 }
             ]
         });
-        res.status(200).json(taDaData);
+
+        const childTaDas = taDaData.get({ plain: true });
+
+        console.log(childTaDas);
+
+        res.render("childidtadas", {
+            ...childTaDas,
+            logged_in: true,
+          });
     } catch (err) {
     res.status(500).json(err);
     }
