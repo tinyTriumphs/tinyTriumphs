a5f207a56034bc70ff2619e5907a2f7988be3138
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-19 00:30:42 -0500

    bulk create now occurse when a child is created

--- controllers/api/childRoutes.js
+++ controllers/api/childRoutes.js
@@ -68,31 +68,43 @@ router.post("/", withAuth, async (req, res) => {
     `);
     console.log(childs.dataValues.id);
 
-    console.log(medMilestoneSeeds, `
+    // console.log(medMilestoneSeeds, `
     
-    med milestone seeds
+    // med milestone seeds
     
-    `)
+    // `)
 
-    const medical = await medMilestones.bulk({
-      medMilestone: 'Sample Milestone',
-      medMilestone_complete: false,
-      medMilestone_ToDoDate: '2022-01-10',
-      child_id: childs.dataValues.id
-    })
+    //all medical fields that will be pre-filled and attached to new child id
+    const medical = await medMilestones.bulkCreate(
+      [
+        { medMilestone: 'I was born!', medMilestone_complete: true, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: 'birth', child_id: childs.dataValues.id },
+        { medMilestone: 'HepB (Hepatitis B)', medMilestone_complete: false, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: 'birth', child_id: childs.dataValues.id },
+        { medMilestone: 'HepB (Hepatitis B), 2nd Dose', medMilestone_complete: false, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: '1-2 months', child_id: childs.dataValues.id },
+        { medMilestone: 'RV (Rotavirus)', medMilestone_complete: false, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: '2 months', child_id: childs.dataValues.id },
+        { medMilestone: 'Dtap (Diphtheria, Tetanus, & Acellular Pertussis)', medMilestone_complete: false, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: '2 months', child_id: childs.dataValues.id }
+      ]
+    )
 
-    const developmental = await devMilestones.create({
-      devMilestone: 'Sample Dev Milestone',
-      devMilestone_complete: false,
-      devMilestone_expectedDate: '2022-01-10',
-      devMilestone_expectedRange: '4-6 months'
-    })
+    const developmental = await devMilestones.bulkCreate(
+      [
+        { devMilestone: 'Took first breath', devMilestone_complete: true, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: 'birth', child_id: childs.dataValues.id },
+        { devMilestone: 'Calms down when spoken to or picked up', devMilestone_complete: false, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: '2-3 months', child_id: childs.dataValues.id },
+        { devMilestone: 'Looks at your face', devMilestone_complete: false, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: '2-3 months', child_id: childs.dataValues.id },
+        { devMilestone: 'Smiles when you talk to or smile at them', devMilestone_complete: false, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: '2-3 months', child_id: childs.dataValues.id },
+      ]
+    )
 
     console.log(medical, `
     
     THIS IS THE MEDICAL DATA
     
     `);
+
+    // console.log(developmental, `
+    
+    // THIS IS THE DEV DATA
+    
+    // `);
 
     res.status(200).json(childs);
   } catch (err) {
