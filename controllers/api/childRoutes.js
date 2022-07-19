33be71de0b0c699991d3d41b7fb5d553dc98844f
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-19 16:09:57 -0500

    Updated models so  that birthdate renders correctly on tadas, applied moment.js to tadas, and updated some styling

--- controllers/api/childRoutes.js
+++ controllers/api/childRoutes.js
@@ -42,16 +42,9 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
+//Route to create a new child
 router.post("/", withAuth, async (req, res) => {
   try {
-    console.log(
-      { ...req.body },
-      `HERE IS THE REQ BODY
-        
-        
-        
-        `
-    );
     const childs = await Child.create({
       ...req.body,
       user_id: req.session.user_id,
@@ -60,24 +53,45 @@ router.post("/", withAuth, async (req, res) => {
       res.status(404).json({ message: "No child found with this id!" });
       return;
     }
+
     console.log(childs, `
     
     
-    HERE ARE THE CHILD DATA
+    HERE ARE THE CHILDS
+    
+    
     
     `);
-    console.log(childs.dataValues.id);
 
-    // console.log(medMilestoneSeeds, `
+    console.log(childs.dataValues.birthdate, `
     
-    // med milestone seeds
+
+
+
+    HERE IS THE BIRTHDATE
     
-    // `)
+
+
+
+    `)
+
+    // var childBirthdate = JSON.stringify(childs.dataValues.birthdate);
+
+    // console.log(childBirthdate, `
+    
+
+
+    // HERE IS THE STRINGIFIED VERSION
+
+
+    
+    // `);
+
 
     //all medical fields that will be pre-filled and attached to new child id
     const medical = await medMilestones.bulkCreate(
       [
-        { medMilestone: 'I was born!', medMilestone_complete: true, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: 'birth', child_id: childs.dataValues.id },
+        { medMilestone: 'I was born!', medMilestone_complete: true, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: 'birth', medMilestone_dateComplete: childs.dataValues.birthdate, child_id: childs.dataValues.id },
         { medMilestone: 'HepB (Hepatitis B)', medMilestone_complete: false, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: 'birth', child_id: childs.dataValues.id },
         { medMilestone: 'HepB (Hepatitis B), 2nd Dose', medMilestone_complete: false, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: '1-2 months', child_id: childs.dataValues.id },
         { medMilestone: 'RV (Rotavirus)', medMilestone_complete: false, medMilestone_ToDoDate: '2022-01-10', medMilestone_expectedRange: '2 months', child_id: childs.dataValues.id },
@@ -102,9 +116,10 @@ router.post("/", withAuth, async (req, res) => {
       ]
     )
 
+    //all developmental fields that will be pre-filled and attached to new child id
     const developmental = await devMilestones.bulkCreate(
       [
-        { devMilestone: 'Took first breath', devMilestone_complete: true, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: 'birth', child_id: childs.dataValues.id },
+        { devMilestone: 'Took first breath', devMilestone_complete: true, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: 'birth', devMilestone_dateComplete: childs.dataValues.birthdate, child_id: childs.dataValues.id },
         { devMilestone: 'Calms down when spoken to or picked up', devMilestone_complete: false, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: '2-3 months', child_id: childs.dataValues.id },
         { devMilestone: 'Looks at your face', devMilestone_complete: false, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: '2-3 months', child_id: childs.dataValues.id },
         { devMilestone: 'Smiles when you talk to or smile at them', devMilestone_complete: false, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: '2-3 months', child_id: childs.dataValues.id },
@@ -194,18 +209,6 @@ router.post("/", withAuth, async (req, res) => {
         { devMilestone: 'Hops on one foot', devMilestone_complete: false, devMilestone_expectedDate: '2022-01-10', devMilestone_expectedRange: '60+ months', child_id: childs.dataValues.id },
       ]
     )
-
-    console.log(medical, `
-    
-    THIS IS THE MEDICAL DATA
-    
-    `);
-
-    // console.log(developmental, `
-    
-    // THIS IS THE DEV DATA
-    
-    // `);
 
     res.status(200).json(childs);
   } catch (err) {
