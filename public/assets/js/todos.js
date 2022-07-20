9ac912f93610707005583f7cd9e66f84ee035dd3
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-07-19 23:44:26 -0500

    medical todos update functional

--- public/assets/js/todos.js
+++ public/assets/js/todos.js
@@ -1,48 +1,8 @@
-// console.log('todos.js is connected!')
-
-// const newFormHandler = async (event) => {
-//     event.preventDefault();
-//     const devDate = document.querySelector('#dev-milestone-date').value.trim();
-//     console.log(devDate);
-//     const devID = document.querySelector('#dev-milestone-id').value;
-//     console.log(devID);
-
-
-//     if (name && childBirthDate && gender) {
-//       const response = await fetch(`/api/children`, {
-//         method: 'POST',
-//         body: JSON.stringify({ name, birthdate: childBirthDate, gender
-//         }),
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         });
-//         console.log(response.body, `
-//         HERE IS THE RESPONSE BODY
-        
-        
-//         `);
-
-//     if (response.ok) {
-//       // console.log(childBirthDate);
-//         document.location.replace('/api/children');
-//       } else {
-//         alert('Failed to create child');
-//     }
-//   }
-// };
-
-// document
-//     .querySelector('.dev-milestone-form')
-//     .addEventListener('click', console.log('button was clicked'));
-
-
 const devModal = $('.devmodal');
-console.log('todos.js connected')
+const medModal = $('.medmodal');
 
-// const photoUpload = $('#photo-upload')
-
-const newFormHandler = async (event) => {
+//Event handler for DEV milestones
+const devMilestoneUpdate = async (event) => {
     event.preventDefault();
 
     event.preventDefault();
@@ -83,11 +43,50 @@ const newFormHandler = async (event) => {
     }
   };
 
-// document
-//     .querySelector('.dev-milestone-form')
-//     .addEventListener('click', newFormHandler)
 
-// document
-//     devModal.add
+//Event handler for MED milestones
+const medMilestoneUpdate = async (event) => {
+    event.preventDefault();
 
-devModal.on("click", ".button", newFormHandler);
+    event.preventDefault();
+    const medDate = document.querySelector('#med-milestone-date').value.trim();
+    const medID = document.querySelector('#med-milestone-id').value;
+
+    
+    var submitButtonClicked = $(event.target);
+    var currentInput = submitButtonClicked.parent()[0];
+    // const selectedFile = document.getElementById('input').files[0].name;
+    // console.log (selectedFile);
+    console.log(submitButtonClicked);
+    console.log('it was clicked');
+    console.log(currentInput);
+    console.log(medDate);
+    // console.log(devID);
+
+    if (medDate && medID) {
+        const response = await fetch(`/api/todos/med`, {
+          method: 'PUT',
+          body: JSON.stringify({ medMilestone_dateComplete: medDate, id: medID }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          });
+          console.log(response, `
+          
+          THIS IS THE RESPONSE ON THE CLIENT
+          
+          `);
+  
+      if (response.ok) {
+        console.log(response);
+        //   document.location.replace('/api/children');
+        } else {
+          alert('Failed to update medMmilestone');
+      }
+    }
+  };
+
+devModal.on("click", ".button", devMilestoneUpdate);
+medModal.on("click", ".button", medMilestoneUpdate);
+
+
